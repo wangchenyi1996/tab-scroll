aeeede2383d17a43b23e40c0c296f1f175357a63
wangchenyi1996 <284518978@qq.com>
2020-05-08 08:51:59 +0800

    Signed-off-by: wangchenyi1996 <284518978@qq.com> 优化在自定义组件上使用vmodel

--- src/components/v_models.vue
+++ src/components/v_models.vue
@@ -1,27 +1,29 @@
 <template class="border">
     <div>
-        子组件:value-{{value}}
-        <!-- <input @input="handleInput" v-model="tempValue"/> -->
-        <input v-model="tempValue" placeholder="请输入..." @input="handleInput"/>
-        <button @click="handleInput">设置值</button>
+        <!--方式一 -->
+        <input placeholder="请输入..." :value="text" @input="$emit('change',$event.target.value)"  />
+
+        <!-- 方式二 -->
+        <input placeholder="请输入..." :value="text" @input="handleInput"  />
     </div>
 </template>
 <script>
 export default {
+    // 注意事项： model.prop.text 要和 props里面的 text一致
+    // model.prop.event 要和 $emit事件一致
+    model:{
+        prop:'text',
+        event:'change'
+    },
     props: {
-        value: {
+        text: {
             type: String,
             default: ''
         }
     },
-    data() {
-        return {
-            tempValue: ''
-        }
-    },
     methods: {
-       handleInput(){
-            this.$emit('input', this.tempValue); //触发 input 事件，并传入新值
+       handleInput(e){
+            this.$emit('change', e.target.value); //触发 input 事件，并传入新值
        }
     }
    
