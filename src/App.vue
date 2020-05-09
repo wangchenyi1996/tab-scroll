6d172ed973fa9531a605eb19ce938c3579a466be
wangchenyi1996 <284518978@qq.com>
2020-05-09 10:21:42 +0800

    Signed-off-by: wangchenyi1996 <284518978@qq.com> provide / inject 高阶组件使用

--- src/App.vue
+++ src/App.vue
@@ -10,11 +10,24 @@
           isShow:true
         }
       },
+      /*
+      provide：是一个对象，或者是一个返回对象的函数。里面呢就包含要给子孙后代的东西，也就是属性和属性值。
+      注意：子孙层的provide会掩盖祖父层provide中相同key的属性值
+
+      inject：一个字符串数组，或者是一个对象。属性值可以是一个对象，包含from和default默认值，
+      from是在可用的注入内容中搜索用的 key (字符串或 Symbol)，意思就是祖父多层provide提供了很多数据，from属性指定取哪一个key;default指定默认值。
+    */
+
       provide(){
         return{
           reload:this.pageReload,
           userInfo:{
-            name:'张小三',
+            name:'张三',
+            age:18,
+            gender:'男'
+          },
+          userInfo1:{
+            name:'张三',
             age:18,
             gender:'男'
           }
--- src/views/About.vue
+++ src/views/About.vue
@@ -37,12 +37,31 @@ export default {
   // inject:["reload",'userInfo'],  
 
   //对象方式
-  inject:{
-    reload:{
-      defalut:'reload'
+  // inject:{
+  //   reload:{
+  //     defalut:'reload'
+  //   },
+  //   userInfo:{
+  //     defalut:'userInfo'
+  //   }
+  // },
+
+   /*
+      provide：是一个对象，或者是一个返回对象的函数。里面呢就包含要给子孙后代的东西，也就是属性和属性值。
+      注意：子孙层的provide会掩盖祖父层provide中相同key的属性值
+
+      inject：一个字符串数组，或者是一个对象。属性值可以是一个对象，包含from和default默认值，
+      from是在可用的注入内容中搜索用的 key (字符串或 Symbol)，意思就是祖父多层provide提供了很多数据，from属性指定取哪一个key;default指定默认值。
+    */
+
+  inject: {
+    reload: {
+      from:'reload',
+      default: () => {}
     },
     userInfo:{
-      defalut:'userInfo'
+      from:'userInfo1',
+      defalut:() => {}
     }
   },
 
